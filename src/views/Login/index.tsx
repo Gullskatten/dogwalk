710f0512b24b7bf95bed62b7f2fdf49b9a480626
oyvindhermansen <oyvind@ludensreklame.no>
2019-11-05 22:21:27 +0100

    minor fix of padding in login view

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import styled from 'styled-components';
 import useAuth from '../../hooks/useAuth';
 import { Button } from '../../components/Button';
-import { HorizontalSpacer, VerticalSpacer } from '../../components/Spacers';
 import Branding from '../../components/Branding';
 import { UserContext } from '../../context/User';
 import Text from '../../components/Text';
@@ -20,22 +19,18 @@ const Login: React.FunctionComponent = () => {
   const { login } = useAuth();
 
   return (
-    <HorizontalSpacer>
-      <VerticalSpacer>
-        <Centerer>
-          {userContext.data.loggingIn ? (
-            <Text variant="subtitle">Logging in....</Text>
-          ) : (
-            <>
-              <Branding />
-              <Button variant="secondary" gutterTop onClick={login}>
-                Log in with Google
-              </Button>
-            </>
-          )}
-        </Centerer>
-      </VerticalSpacer>
-    </HorizontalSpacer>
+    <Centerer>
+      {userContext.data.loggingIn ? (
+        <Text variant="subtitle">Logging in....</Text>
+      ) : (
+        <>
+          <Branding />
+          <Button variant="secondary" gutterTop onClick={login}>
+            Log in with Google
+          </Button>
+        </>
+      )}
+    </Centerer>
   );
 };
 
